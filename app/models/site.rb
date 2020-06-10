fc632d4741ec8bbb9d5e10482708a91fc98fbf05
Allison Mackey <a.mackey693@gmail.com>
2020-06-10 16:14:05 -0700

    finalize changes for the day

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,6 +3,10 @@ class Site
     response = HTTParty.get('https://boiling-brook-46170.herokuapp.com/quotes')
   end
 
+  # def self.get_quote(quote_id)
+  #   response = HTTParty.get("https://boiling-brook-46170.herokuapp.com/quotes/#{quote_id}")
+  # end
+
   def self.search_quotes(search_term)
     response = HTTParty.get("https://boiling-brook-46170.herokuapp.com/search?query=#{search_term}")
   end
